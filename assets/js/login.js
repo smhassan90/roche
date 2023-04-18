ffe76cabf8254dfe45411026c1204f59e3138a4f
Moin Ahmed Ansari <moinahmed@greenstar.org.pk>
2023-04-18 14:29:20 +0500

    id created for sana danish

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,7 +12,12 @@ $(document).ready(function(){
         if(id==='kiran' && pass==='kiran123'){
             swal("Successfully!", "Kiran, You are logged in", "success").done();
             self.location="side_menu.html";
-        }else{
+        }else if(id==='sana' && pass==='sana123'){
+                         swal("Successfully!", "Sana, You are logged in", "success").done();
+                         self.location="side_menu.html";
+        }else
+
+        {
             swal("Error!", "Invalid username or password!", "error").done();
         }
     });
